c7f4cf0e4fcf50021e60b49974b344a6c4723c8a
ImkeSmit <imke610@gmail.com>
2024-04-03 12:58:54 +0200

    make minor changes eg filenames

--- scripts/Functional distance analysis.R
+++ scripts/Functional distance analysis.R
@@ -5,9 +5,11 @@ library(ggplot2)
 library(glmmTMB)
 library(car)
 library(emmeans)
+library(DHARMa)
+library(MuMIn)
 
 #From the filled trait data for plotspecific species
-FT <- read.csv("Functional trait data\\FT_filled_match_facilitation_plots_plotspecific_species_7Feb2024.csv", row.names = 1)
+FT <- read.csv("Functional trait data\\Clean data\\FT_filled_match_facilitation_plots_plotspecific_species.csv", row.names = 1)
 
 ##We need to standardise each trait value to mean 0 and variance of 1 with Z = (x-mean)/sd
 ##We need only one trait value per species
@@ -161,12 +163,12 @@ for (t in 1:length(countrynames)) {
 
 sp_positions <- as.data.frame(sp_positions)
 #save the table 
-write.csv(sp_positions, "Functional trait data//sp_positions.csv")
+write.csv(sp_positions, "Functional trait data\\results\\sp_positions.csv")
 
 
 ###Retreive distance between pairs from distance matrix####
 #import sp_positions
-sp_positions <- read.csv("Functional trait data//sp_positions.csv", row.names = 1) 
+sp_positions <- read.csv("Functional trait data\\results\\sp_positions.csv", row.names = 1) 
 
 IDlist <- unique(sp_positions$ID)
 
@@ -219,7 +221,7 @@ for(i in 1:length(IDlist)) {
   }
 }
 #The distance is NaN if nurse_only, bare_only or both is NA
-write.csv(pair_dist, "Functional trait data\\Results\\Functional_distances.csv")
+write.csv(pair_dist, "Functional trait data\\results\\Functional_distances.csv")
 
 
 ##Now we need to add the aridity, graz of each plot, and the nintc of each rep
@@ -282,7 +284,13 @@ mod2 <- glmmTMB(NIntc_richness_binom ~ Delta_nurse_bare + (1|SITE_ID), family = 
 Anova(mod2)
 summary(mod2)
 
-#Is does the functional distance depend on whether the distance is between nurse and nurse_only species, or nurse and bare_only species, or nurse and both species
+mod3 <- glmmTMB(NIntc_richness_binom ~ D_bare_only + (1|SITE_ID), family = "binomial", data = pair_dist)
+Anova(mod3)
+summary(mod2)
+
+
+
+#Does the functional distance depend on whether the distance is between nurse and nurse_only species, or nurse and bare_only species, or nurse and both species
 #We need to change the pair_dist data to long format
 
 long_pair_dist <- pair_dist |> 
@@ -291,23 +299,33 @@ long_pair_dist <- pair_dist |>
   pivot_longer(cols = c(D_bare_only, D_nurse_only, D_both), names_to = "grouping", values_to = "mean_Fdist")
 long_pair_dist$grouping <- as.factor(long_pair_dist$grouping)
 
-mod3 <- glmmTMB(mean_Fdist ~ grouping + GRAZ + ARIDITY.v3 + (1|SITE_ID), data = long_pair_dist)
-Anova(mod3)
+mod4 <- glmmTMB(mean_Fdist ~ grouping + GRAZ + ARIDITY.v3 + (1|SITE_ID), data = long_pair_dist)
+Anova(mod4)
 #Graz significant
-summary(mod3)
-emmeans(mod3, specs = c("GRAZ")) #?? does not agree with summary table
+summary(mod4)
+emmeans(mod4, specs = c("GRAZ")) #?? does not agree with summary table
 
 #without random effect
-mod4 <- glmmTMB(mean_Fdist ~ grouping + GRAZ + ARIDITY.v3 , data = long_pair_dist)
-Anova(mod4)
+mod5 <- glmmTMB(mean_Fdist ~ grouping + GRAZ + ARIDITY.v3 , data = long_pair_dist)
+Anova(mod5)
 #now graz and aridity is significant
+summary(mod5)
+emmeans(mod5, specs = "GRAZ")
+
+
+mod6 <- glmmTMB(mean_Fdist ~ grouping*GRAZ*ARIDITY.v3 + (1|SITE_ID), data = long_pair_dist)
+Anova(mod6)
+#Graz significant
 summary(mod4)
-emmeans(mod4, specs = "GRAZ")
+emmeans(mod4, specs = c("GRAZ")) #?? does not agree with summary table
 
 ggplot(long_pair_dist, aes(x = mean_Fdist)) +
   geom_histogram()
 
 ggplot(long_pair_dist, aes(x = GRAZ, y = mean_Fdist)) +
+  geom_boxplot() 
+
+ggplot(long_pair_dist, aes(x = grouping, y = mean_Fdist, fill = GRAZ)) +
   geom_boxplot()
 
 ggplot(long_pair_dist, aes(x = grouping, y = mean_Fdist)) +
@@ -322,3 +340,192 @@ long_pair_dist <- long_pair_dist |>
 
 ggplot(long_pair_dist, aes(x = arid_bin, y = mean_Fdist)) +
   geom_boxplot()
+
+
+###Another way: retreive distances between the nurse and each species also growing in the rep####
+#then classify that distance as D_bare_only, D_nurse_only or D_both
+#This distance is only between 2 sp, not between the nurse and many sp as in pair_dist
+
+#import sp_positions
+sp_positions <- read.csv("Functional trait data\\results\\sp_positions.csv", row.names = 1) 
+
+IDlist <- unique(sp_positions$ID)
+
+twosp_dist <- cbind("ID" = NA, "replicate" = NA, 
+                    "euclidean_dist" = NA, "grouping" = NA)
+
+for(i in 1:length(IDlist)) {
+  plot <- sp_positions |> 
+    filter(ID == IDlist[i])
+  
+  replist <- unique(plot$replicate) 
+  
+  for(r in 1:length(replist)) {
+    #Get the names of nurse, bare only, nurse only and both species
+    NURSE <- sp_positions |> 
+      filter(ID == IDlist[i], replicate == replist[r], position == "nurse_species") |> 
+      select(taxon)
+    NURSE <- NURSE$taxon
+    
+    bare_only <- sp_positions |> 
+      filter(ID == IDlist[i], replicate == replist[r], position == "bare_only") |> 
+      select(taxon)
+    bare_only <- c(bare_only$taxon)
+    
+    nurse_only <- sp_positions |> 
+      filter(ID == IDlist[i], replicate == replist[r], position == "nurse_only") |> 
+      select(taxon)
+    nurse_only <- c(nurse_only$taxon)
+    
+    both <- sp_positions |> 
+      filter(ID == IDlist[i], replicate == replist[r], position == "both") |> 
+      select(taxon)
+    both <- c(both$taxon)
+    
+    ###
+    for(b in 1:length(bare_only)) {
+      D_bare_only <- 
+      mean(distmat[which(rownames(distmat) == NURSE), which(colnames(distmat) == bare_only[b])])
+      
+      if(b == 1){
+        twosp_bare_only <- cbind("ID" = IDlist[i], "replicate" = replist[r], 
+                                 "euclidean_dist" = D_bare_only, "grouping" = "nurse_bare_only")
+      } else {
+        temp_twosp_bare_only <- cbind("ID" = IDlist[i], "replicate" = replist[r], 
+                                      "euclidean_dist" = D_bare_only, "grouping" = "nurse_bare_only")
+        twosp_bare_only <- rbind(twosp_bare_only, temp_twosp_bare_only)
+      }}
+      
+    ###
+    for(n in 1:length(nurse_only)) {
+      D_nurse_only <- 
+        mean(distmat[which(rownames(distmat) == NURSE), which(colnames(distmat) == nurse_only[n])])
+      
+      if(n == 1){
+        twosp_nurse_only <- cbind("ID" = IDlist[i], "replicate" = replist[r], 
+                                 "euclidean_dist" = D_nurse_only, "grouping" = "nurse_nurse_only")
+      } else {
+        temp_twosp_nurse_only <- cbind("ID" = IDlist[i], "replicate" = replist[r], 
+                                      "euclidean_dist" = D_nurse_only, "grouping" = "nurse_nurse_only")
+        twosp_nurse_only <- rbind(twosp_nurse_only, temp_twosp_nurse_only)
+      }}
+      
+    ###
+    for(z in 1:length(both)) {
+      D_both <- 
+        mean(distmat[which(rownames(distmat) == NURSE), which(colnames(distmat) == both[z])])
+      
+      if(z == 1){
+        twosp_both <- cbind("ID" = IDlist[i], "replicate" = replist[r], 
+                                  "euclidean_dist" = D_both, "grouping" = "nurse_both")
+      } else {
+        temp_twosp_both <- twosp_both <- cbind("ID" = IDlist[i], "replicate" = replist[r], 
+                                                     "euclidean_dist" = D_both, "grouping" = "nurse_both")
+        twosp_both <- rbind(twosp_both, temp_twosp_both)
+      }}
+      
+      twosp_dist <- rbind(twosp_dist, twosp_bare_only,twosp_nurse_only, twosp_both)
+  }
+}
+#The distance is NaN if nurse_only, bare_only or both is NA
+
+#Add ardidty and graz
+twosp_dist <- as.data.frame(twosp_dist)
+twosp_dist$ID <- as.numeric(twosp_dist$ID)
+#import siteinfo
+siteinfo <- read.csv("C:\\Users\\imke6\\Documents\\Msc Projek\\Facilitation analysis\\Facilitation data\\BIODESERT_sites_information.csv") |> 
+  select(ID,COU,SITE,SITE_ID,PLOT,GRAZ, ARIDITY.v3) |> 
+  distinct() |> 
+  filter(!is.na(ID))
+
+#do the join
+twosp_dist <- twosp_dist |> 
+  filter(!is.na(euclidean_dist), !euclidean_dist == "NaN") |> 
+  inner_join(siteinfo, by = "ID") 
+
+write.csv(twosp_dist, "Functional trait data\\results\\Functional_distances_between_2sp.csv")
+
+
+###Make some models
+twosp_dist <- read.csv("Functional trait data\\results\\Functional_distances_between_2sp.csv", row.names = 1)
+twosp_dist$GRAZ <- as.factor(twosp_dist$GRAZ)
+twosp_dist$SITE_ID <- as.factor(twosp_dist$SITE_ID)
+twosp_dist$grouping <- as.factor(twosp_dist$grouping)
+twosp_dist$arid_sq <- (twosp_dist$ARIDITY.v3)^2
+
+
+ggplot(twosp_dist, aes(x = euclidean_dist)) +
+  geom_histogram()
+
+#model without interactions
+twosp1 <- glmmTMB(euclidean_dist ~ grouping +GRAZ + ARIDITY.v3 + arid_sq 
+                     + (1|SITE_ID), data = twosp_dist)
+summary(twosp1)
+Anova(twosp1) #only grazing is significant
+AIC(twosp1) #36140.13
+
+
+
+#model with interactions 
+twosp2 <- glmmTMB(euclidean_dist ~ grouping*GRAZ +grouping*ARIDITY.v3 + 
+                    ARIDITY.v3*GRAZ + grouping*arid_sq + arid_sq*GRAZ + (1|SITE_ID), data = twosp_dist)
+Anova(twosp2)
+summary(twosp2)
+AIC(twosp2) #35883.66
+emmeans(twosp2, specs = "grouping")
+emmeans(twosp2, specs = "GRAZ")
+
+twosp2_simres <- simulateResiduals(twosp2)
+plot(twosp2_simres)
+
+twosp_null <- glmmTMB(euclidean_dist ~ 1 + (1|SITE_ID), data = twosp_dist)
+anova(twosp_null, twosp2) #whole model p = < 2.2e-16
+
+#get R squared
+r.squaredGLMM(twosp2) #take the theoretical
+
+
+##Let's run the model only for replicates with Nintc_richness > 0
+nint_result <- 
+  read.csv("C:\\Users\\imke6\\Documents\\Msc Projek\\Facilitation analysis\\Facilitation data\\results\\NIntc_results_allcountries_6Feb2024.csv", row.names =1) |> 
+  filter(ID %in% c(twosp_dist$ID)) |>
+  filter(!is.na(NIntc_richness)) |> 
+  select(!c(NIntc_cover, NInta_richness, NInta_cover, NIntc_shannon, NInta_shannon)) |> 
+  mutate(ID_rep = str_c(ID, replicate_no, sep = "_")) |> 
+  mutate(interaction = case_when(NIntc_richness > 0 ~ "facilitation", 
+                                 NIntc_richness < 0 ~ "competition", 
+                                 NIntc_richness == 0 ~ "neutral")) |> 
+  select(ID_rep, interaction)
+
+#now isolate facilitative reps in twosp_dist
+twosp_dist_fac <- twosp_dist |> 
+  mutate(ID_rep = str_c(ID, replicate, sep = "_")) |> 
+  left_join(nint_result, by = "ID_rep")  |> 
+  filter(interaction == "facilitation")
+
+#model for only facilitative interactions
+twosp_fac <- glmmTMB(euclidean_dist ~ grouping*GRAZ +grouping*ARIDITY.v3 + 
+                       ARIDITY.v3*GRAZ + grouping*arid_sq + arid_sq*GRAZ + (1|SITE_ID), data = twosp_dist)
+
+Anova(twosp_fac)
+summary(twosp_fac)
+  
+
+
+#PLOTS#
+#dist ~ graz
+ggplot(twosp_dist, aes(x = GRAZ, y = euclidean_dist)) +
+  geom_boxplot()
+
+#dist~graz, colour by grouping
+ggplot(twosp_dist, aes(x = GRAZ, y = euclidean_dist, fill = grouping)) +
+  geom_boxplot()
+
+#dist~grouping, colour by graz
+ggplot(twosp_dist, aes(x = grouping, y = euclidean_dist, fill = GRAZ)) +
+  geom_boxplot()
+
+
+#dist~aridity
+ggplot(twosp_dist , aes(x = ARIDITY.v3, y = euclidean_dist, color = grouping)) +
+  geom_jitter(width = 0.1, height = 0.1)
