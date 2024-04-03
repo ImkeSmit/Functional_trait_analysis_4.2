5d00e14ec1aedd33946da15f38f90238d9743b77
ImkeSmit <imke610@gmail.com>
2024-04-03 13:49:47 +0200

    standardise FT values and perform NMDS

--- scripts/NMDS working.R
+++ scripts/NMDS working.R
@@ -4,3 +4,78 @@ library(vegan)
 library(tidyverse)
 library(tidylog)
 library(ggplot)
+
+set.seed(100)
+
+#load data
+#From the filled trait data for plotspecific species
+FT <- read.csv("Functional trait data\\Clean data\\FT_filled_match_facilitation_plots_plotspecific_species.csv", row.names = 1)
+##!remember that these species were not necessarily filled from the same graz level
+
+##We need to standardise each trait value to mean 0 and variance of 1 with Z = (x-mean)/sd
+##We need only one trait value per species
+
+#Get the mean of each trait for each sp
+FT_mean <- FT |> 
+  group_by(taxon,trait) |> 
+  summarise(mean_value = mean(value))
+
+#Get it into wide format
+FT_wide <- FT_mean |>
+  pivot_wider(names_from = trait, values_from = mean_value) |> 
+  column_to_rownames(var = "taxon") |> 
+  filter(!is.na(MaxH),
+         !is.na(MaxLS), 
+         !is.na(MeanLA),
+         !is.na(MeanLDMC),
+         !is.na(MeanLL),
+         !is.na(MeanSLA), 
+         !is.na(percentC),
+         !is.na(percentN)) |> 
+  mutate(C_N_ratio = percentC/percentN) |> 
+  select(!c(percentC, percentN))
+
+
+
+#standardise trait values
+std_FT_wide <- FT_wide
+
+traitlist <- c("MaxH","MaxLS","MeanLA","MeanLDMC","MeanLL","MeanSLA","C_N_ratio")
+
+for(t in 1:length(traitlist)) {
+  
+  #get the grand mean and sd for each trait
+  m <- FT_wide |> 
+    summarise(m = mean(FT_wide[ , which(colnames(FT_wide) == traitlist[t])]))
+  
+  sd <- FT_wide |> 
+    summarise(m = sd(FT_wide[ , which(colnames(FT_wide) == traitlist[t])]))
+  
+  for (i in 1:nrow(FT_wide)) {
+    
+    raw_value <- FT_wide[i , which(colnames(FT_wide) == traitlist[t])]
+    #replace the raw value with the standardised value using m and sd
+    std_FT_wide[i , which(colnames(std_FT_wide) == traitlist[t])] <- (raw_value - m)/sd
+    
+  }
+}
+
+#do NMDS
+test_nmds <- metaMDS(std_FT_wide, distance = "euclidean")
+#Extract NMDS Scores 
+nmds_scores <-as.data.frame(scores(test_nmds))
+
+ggplot(nmds_scores, aes(NMDS1, NMDS2)) +
+  geom_point()
+
+
+#Now let's get the species that occur in each grazing level, and which of those are nurses and targets
+#all the species in the trait data
+sp_graz <- FT |> 
+  group_by(GRAZ) |> 
+  distinct(taxon) |> 
+  ungroup()
+
+
+##add the grazing levels to std FT wide
+
