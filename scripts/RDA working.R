a034ebb420724924e3eafdcfd87583c591126a2e
ImkeSmit <imke610@gmail.com>
2024-04-08 10:56:23 +0200

    annotate loop

--- scripts/RDA working.R
+++ scripts/RDA working.R
@@ -28,62 +28,67 @@ FT <- read.csv("Functional trait data\\Clean data\\FT_filled_match_facilitation_
                row.names = 1) |> 
   filter(COU == "South Africa")
 
-traits_collected <- c(unique(FT$trait))
+###Loop to make sp x trait matrices for each nurse microsite####
 
-#Now we need to assemble species by trait matrices for every nurse microsite in southafrica
+#names of the traits collectd
+traits_collected <- c(unique(FT$trait))
+#plot Id's
 IDlist <- c(unique(sa$ID))
+#empty list of matrices
 matlist <- list()
 
 l = 1
 for (i in 1:length(IDlist)) {
-  fac_plot <- sa[which(sa$ID == IDlist[i]) , ]
   
+  #isolate a plot in the facilitation and FT data
+  fac_plot <- sa[which(sa$ID == IDlist[i]) , ]
   FT_plot <- FT[which(FT$ID == IDlist[i]) , ]
   
+  #list of reps in this plot
   replist <- c(unique(fac_plot$Number.of.replicate))
   
+  #get the species that occur in the nurse microsite of every rep
   for (r in 1:length(replist)) {
     one_rep <- fac_plot[which(fac_plot$Number.of.replicate == replist[r]) , ]
+    comm_sp <- c(one_rep$Species.within.quadrat) #species names
     
-    comm_sp <- c(one_rep$Species.within.quadrat)
-    
+    #create a vector of ID_rep to identify each matrix. we will use this to name the list at the end of the loop
     if(l == 1) {comm_index <- paste(IDlist[i], replist[r], sep = "_")}
     else{comm_index_temp <- paste(IDlist[i], replist[r], sep = "_")
         comm_index <- c(comm_index, comm_index_temp)}
     
+    #create an empty sp x trait matrix
     comm_mat <- matrix(nrow = length(comm_sp), ncol = length(traits_collected))
     row.names(comm_mat) <- comm_sp
     colnames(comm_mat) <- traits_collected
     
     #get the trait vals of species in comm_mat
-    for(s in 1:length(comm_sp)) {
-      for(t in 1:length(traits_collected)) {
+    for(s in 1:length(comm_sp)) { #for each species
+      for(t in 1:length(traits_collected)) { #and each trait
+      
+      #isolate the trait value(s) for each species and trait  
+      val <- FT_plot |> 
+        filter(taxon == comm_sp[s], 
+               trait == traits_collected[t]) |> 
+        select(value) 
     
-    val <- FT_plot |> 
-      filter(taxon == comm_sp[s], 
-             trait == traits_collected[t]) |> 
-      select(value) 
+      #if there are no values for this trait, put NA in the matrix
+      if(nrow(val) == 0) {comm_mat[s,t] <- NA} 
+        #if there are 2 or more trait measurements, get the mean and put that in the matrix
+        else if (nrow(val) > 1) {mean_val <- mean(val$value)
+          comm_mat[s,t] <- mean_val} 
+          #if there is only on trait value, put that in the matrix
+          else {comm_mat[s,t] <- val$value} 
+      }#end loop through traits
+    }#end loop through species
     
-    if(nrow(val) == 0) {comm_mat[s,t] <- NA} #if there are no values for this trait, put NA in the matrix
-    else if (nrow(val) > 1) {mean_val <- mean(val$value)#if there are 2 or more trait measurements, get the mean and put that in the matrix
-    comm_mat[s,t] <- mean_val} 
-    else {comm_mat[s,t] <- val$value} #if there is only on trait value, put that in the matrix
-    
-      }
-    }
-    
-    matlist[[l]] <- comm_mat
+    matlist[[l]] <- comm_mat #put filled matrix in the list
 
     l = l+1
-  }
-}
-names(matlist) <- comm_index
+  }#end loop through reps
+}#end loop through plots
+names(matlist) <- comm_index #name the list elements
 
 
-#now we need to fill these matrices with traits
-for (i in 1:length(IDlist)) {
-  FT_plot <- FT[which(FT$ID == IDlist[i]) , ]
-  
-  
-}
+
 
