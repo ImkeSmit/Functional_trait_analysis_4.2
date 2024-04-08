6858cfee3e2c788c5c84dd524c74e3f03e2716a7
ImkeSmit <imke610@gmail.com>
2024-04-08 09:53:49 +0200

    import data

--- scripts/RDA working.R
+++ scripts/RDA working.R
@@ -1,2 +1,25 @@
 ###RDA:###
-###Do the response traits of associated communities depend on the effect traits of dominnat plants###
+###Do the response traits of associated communities depend on the effect traits of dominant plants###
+library(tidyverse)
+library(tidylog)
+library(vegan)
+
+##Import raw countries data
+data_files <- list.files("C:\\Users\\imke6\\Documents\\Msc Projek\\Facilitation analysis\\Facilitation data\\Countriesv3")
+countrynames <- c("algeria", "argentina", "australia", "chile", "chinachong", "chinaxin", "iranabedi", "iranfarzam", 
+                  "israel", "namibiablaum", "namibiawang", "southafrica",  "spainmaestre", "spainrey")
+for(i in 1:length(data_files)) {                              
+  assign(paste0(countrynames[i]),                                   
+         read.csv2(paste0("C:\\Users\\imke6\\Documents\\Msc Projek\\Facilitation analysis\\Facilitation data\\Countriesv3\\",
+                          data_files[i])))
+}
+
+##lets only work with Southafrica for now
+
+
+##Import trait data
+#From the filled trait data for plotspecific species
+##!remember that these species were not necessarily filled from the same graz level
+FT <- read.csv("Functional trait data\\Clean data\\FT_filled_match_facilitation_plots_plotspecific_species.csv",
+               row.names = 1) |> 
+  filter(COU == "South Africa")
