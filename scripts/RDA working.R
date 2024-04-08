a0d2af5c0f5942251e974d87c2b741289c6e0d7f
ImkeSmit <imke610@gmail.com>
2024-04-08 10:25:21 +0200

    write the loop to create empty species x trait matrices

--- scripts/RDA working.R
+++ scripts/RDA working.R
@@ -15,6 +15,10 @@ for(i in 1:length(data_files)) {
 }
 
 ##lets only work with Southafrica for now
+#only work with nurse microsites
+sa <- southafrica |> 
+  filter(Microsite == 2, 
+         !is.na(Species.within.quadrat))
 
 
 ##Import trait data
@@ -23,3 +27,35 @@ for(i in 1:length(data_files)) {
 FT <- read.csv("Functional trait data\\Clean data\\FT_filled_match_facilitation_plots_plotspecific_species.csv",
                row.names = 1) |> 
   filter(COU == "South Africa")
+
+traits_collected <- c(unique(FT$trait))
+
+#Now we need to assemble species by trait matrices for every nurse microsite in southafrica
+IDlist <- c(unique(sa$ID))
+matlist <- list()
+
+l = 1
+for (i in 1:length(IDlist)) {
+  one_plot <- sa[which(sa$ID == IDlist[i]) , ]
+  
+  replist <- c(unique(one_plot$Number.of.replicate))
+  
+  for (r in 1:length(replist)) {
+    one_rep <- one_plot[which(one_plot$Number.of.replicate == replist[r]) , ]
+    
+    comm_sp <- c(one_rep$Species.within.quadrat)
+    
+    if(l == 1) {comm_index <- paste(IDlist[i], replist[r], sep = "_")}
+    else{comm_index_temp <- paste(IDlist[i], replist[r], sep = "_")
+        comm_index <- c(comm_index, comm_index_temp)}
+    
+    comm_mat <- matrix(nrow = length(comm_sp), ncol = length(traits_collected))
+    row.names(comm_mat) <- comm_sp
+    colnames(comm_mat) <- traits_collected
+    
+    matlist[[l]] <- comm_mat
+
+    l = l+1
+  }
+}
+names(matlist) <- comm_index
