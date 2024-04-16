3864e8b9526d1550da1a74fc750b08f4a0eedcc4
ImkeSmit <imke610@gmail.com>
2024-04-16 12:04:47 +0200

    do analysis with nint result rather than raw country data

--- scripts/nint_trait_models.R
+++ scripts/nint_trait_models.R
@@ -5,28 +5,14 @@ library(tidylog)
 library(vegan)
 library(DescTools)
 
-##Import raw countries data
-data_files <- list.files("C:\\Users\\imke6\\Documents\\Msc Projek\\Facilitation analysis clone\\Facilitation data\\Countriesv3")
-countrynames <- c("algeria", "argentina", "australia", "chile", "chinachong", "chinaxin", "iranabedi", "iranfarzam", 
-                  "israel", "namibiablaum", "namibiawang", "southafrica",  "spainmaestre", "spainrey")
-for(i in 1:length(data_files)) {                              
-  assign(paste0(countrynames[i]),                                   
-         read.csv2(paste0("C:\\Users\\imke6\\Documents\\Msc Projek\\Facilitation analysis\\Facilitation data\\Countriesv3\\",
-                          data_files[i])))
-}
-varnames <- c("ID", "site_ID", "Microsite", "ID_Microsite", "Number.of.replicate", "GRAZ", "ARIDITY.v3", "Species.within.quadrat")
-allcountries <- rbind(algeria[, which(colnames(algeria) %in% varnames)], argentina[, which(colnames(argentina) %in% varnames)],
-                      australia[, which(colnames(australia) %in% varnames)], chile[, which(colnames(chile) %in% varnames)], 
-                      chinachong[, which(colnames(chinachong) %in% varnames)], chinaxin[, which(colnames(chinaxin) %in% varnames)],
-                      iranabedi[, which(colnames(iranabedi) %in% varnames)], iranfarzam[, which(colnames(iranfarzam) %in% varnames)],
-                      israel[, which(colnames(israel) %in% varnames)], namibiablaum[, which(colnames(namibiablaum) %in% varnames)],
-                      namibiawang[, which(colnames(namibiawang) %in% varnames)], southafrica[, which(colnames(southafrica) %in% varnames)],
-                      spainmaestre[, which(colnames(spainmaestre) %in% varnames)], spainrey[, which(colnames(spainrey) %in% varnames)])
-
-#work only with nurse microsites
-allcountries <- allcountries |> 
-  filter(!is.na())
-
+#import nint results
+nint_result <- read.csv("C:\\Users\\imke6\\Documents\\Msc Projek\\Facilitation analysis clone\\Facilitation data\\results\\NIntc_results_allcountries_6Feb2024.csv", row.names = 1) |> 
+  #transform the nint variables to range between 0 and 1
+  mutate(NIntc_richness_binom = (NIntc_richness + 1)/2, 
+         NIntc_cover_binom = (NIntc_cover + 1)/2, 
+         NInta_richness_binom = (NInta_richness - (-1)) / (2 - (-1)), 
+         NInta_cover_binom = (NInta_cover - (-1)) / (2 - (-1)))
+  
 
 ##import trait data
 FT <- read.csv("Functional trait data\\Clean data\\FT_filled_match_facilitation_plots_plotspecific_species.csv",
@@ -42,7 +28,7 @@ FT <- read.csv("Functional trait data\\Clean data\\FT_filled_match_facilitation_
 #names of the traits collectd
 traits_collected <- c(unique(FT$trait))
 #plot Id's
-IDlist <- c(unique(allcountries$ID))
+IDlist <- c(unique(nint_result$ID))
 #empty table with explanatory variables
 modeldat <- data.frame(ID = NA, site_ID = NA, replicate_no = NA, nurse_sp = NA, NIntc_richness = NA, NIntc_cover = NA, NInta_richness = NA, 
                       NInta_cover = NA, aridity = NA, graz = NA, nurse_mean_percentN = NA, nurse_mean_percentC = NA, 
@@ -56,7 +42,7 @@ l = 1
 for (i in 1:length(IDlist)) {
   
   #isolate a plot in the facilitation and FT data
-  fac_plot <- allcountries[which(allcountries$ID == IDlist[i]) , ]
+  fac_plot <- nint_result[which(nint_result$ID == IDlist[i]) , ]
   FT_plot <- FT[which(FT$ID == IDlist[i]) , ]
   
   #list of reps in this plot
@@ -66,10 +52,7 @@ for (i in 1:length(IDlist)) {
   for (r in 1:length(replist)) {
     one_rep <- fac_plot[which(fac_plot$Number.of.replicate == replist[r]) , ]
     #get the nurse species
-    nurse_sp <- unique(one_rep$ID_Microsite)
-    
-    #concatenate ID and rep to make an identifier
-    comm_index <- paste(IDlist[i], replist[r], sep = "_")
+    nurse_sp <- one_rep$nurse
     
     #get the trait values of the nurse species
     for (t in 1:length(traits_collected)) {
