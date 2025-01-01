5c360c7a7c6c663553a6e86897107d6f12941db7
ImkeSmit <imke610@gmail.com>
2025-01-01 11:48:00 +0200

    clean model data and run stepwise model selection

--- scripts/New analysis for paper/2. Stepwise moelde selection.R
+++ scripts/New analysis for paper/2. Stepwise moelde selection.R
@@ -1,5 +1,8 @@
 ###Script to run stepwise model selection procedure to determine how traits, the environment, 
 ###and their interaction affect interaction outcomes
+library(glmmTMB)
+library(MuMIn)
+library(tidyverse)
 
 #full model: nint ~ nurse_maxh + nurse_ldmc + graz + climate + soil
                   # + graz:climate + graz:soil + soil:climate + climate:climate +
@@ -16,7 +19,7 @@ modeldat$ID <- as.factor(modeldat$ID)
 #import siteinfo, we will use this to add ID to drypop
 siteinfo <- read.csv("C:\\Users\\imke6\\Documents\\Msc Projek\\Facilitation analysis clone\\Facilitation data\\BIODESERT_sites_information.csv") |> 
   mutate(plotref = str_c(SITE, PLOT, sep = "_")) |> 
-  select(ID, plotref) |> 
+  select(ID, plotref, Lat_decimal, Long_decimal) |> 
   distinct() |> 
   na.omit()
 
@@ -37,9 +40,47 @@ modeldat_final <- modeldat |>
          aridity2 = aridity^2) |> 
   #remove all rows which have NA values in any of our modelling variables
   drop_na(NIntc_richness_binom, NIntc_cover_binom,NInta_richness_binom, NInta_cover_binom, 
-          nurse_meanLDMC, nurse_mean_H, aridity, AMT, RASE, SAC, pH, graz)
+          log_nurse_meanLDMC, log_nurse_meanH, aridity, AMT, RASE, SAC, pH, graz)
+
+####define the full model formula ####
+
+full_formula <- as.formula("NIntc_richness_binom ~ 
+    graz + aridity + AMT + RASE + pH + SAC + log_nurse_meanH + log_nurse_meanLDMC +
+    graz:aridity + graz:RASE + graz:AMT + graz:pH + graz:SAC +
+    #pH:aridity + pH:AMT + pH:RASE + SAC:aridity + SAC:AMT + SAC:RASE +
+    RASE:AMT + RASE:aridity + AMT:aridity +
+graz:log_nurse_meanH + graz:log_nurse_meanLDMC +
+aridity:log_nurse_meanH + aridity:log_nurse_meanLDMC +
+AMT:log_nurse_meanH + AMT:log_nurse_meanLDMC +
+RASE:log_nurse_meanH + RASE:log_nurse_meanLDMC +
+pH:log_nurse_meanH + pH:log_nurse_meanLDMC +
+SAC:log_nurse_meanH + SAC:log_nurse_meanLDMC +
+Lat_decimal +Long_decimal + #add these to account for spatial structure instead of (1|site_ID/ID)
+(1|nurse_sp)") 
 
 
 
+# Fit the full model
+options(na.action = "na.omit")
+full_model <- glmmTMB(
+  formula = full_formula,
+  data = modeldat_final,    
+  family = binomial  #had to remove sq terms and soil:climate interactions to make model converge
+)
+
+# Ensure all models maintain random effects by excluding them from being dropped
+options(na.action = "na.fail") # Required for dredge function
+
+# Perform stepwise model selection using dredge
+model_selection <- dredge(
+  full_model, 
+  fixed = c("(1|nurse_sp)", "Long_decimal", "Lat_decimal"),  # Keep random effects in all models
+  rank = "AIC"                                # Use AIC for model ranking
+) #took 50 mins to run
+
+write.csv(model_selection, "Functional trait data\\paper_results\\stepwise_results.csv")
+
+# View model selection table
+print(model_selection)
 
 
