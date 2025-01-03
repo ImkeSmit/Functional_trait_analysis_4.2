64d45ef5ca86711f23560b3c4429dd166b98064d
ImkeSmit <imke610@gmail.com>
2025-01-03 09:53:18 +0200

    create a script for the trait difference analysis

---
+++ scripts/New analysis for paper/3. Trait difference analysis.R
@@ -0,0 +1,4 @@
+##This script is to run the second analysis to determine whther the functional distance between dominant and 
+#target species are affected by target species' association and environmental factors
+
+trait_fdist <- read.csv("Functional trait data\\results\\trait_differences_between_2sp_traits_vary.csv", row.names = 1)
