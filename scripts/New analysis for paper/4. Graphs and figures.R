8b89673a068e94ba152e29a00bc72eacbee1d523
ImkeSmit <imke610@gmail.com>
2025-01-16 13:22:32 +0200

    start new figure showing effects of chosen variables on nintc

--- scripts/New analysis for paper/4. Graphs and figures.R
+++ scripts/New analysis for paper/4. Graphs and figures.R
@@ -1,6 +1,10 @@
 ###Script to make graphs and figures for the paper####
 library(ggplot2)
 library(ggpubr)
+library(tidyverse)
+library(tidylog)
+library(glmmTMB)
+library(MuMIn)
 
 ####AKAIKE WEIGHTS FIGURE####
 #import akaike weights of nint richness model
@@ -28,7 +32,6 @@ rich_sw_bar <- ggplot(aes(x = var2, y = importance), data = rich_sw) +
   coord_flip()+
   theme_classic()
 
-
 cov_sw_bar <- ggplot(aes(x = var2, y = importance), data = cov_sw) +
   geom_bar(stat = "identity") +
   xlab("Variable") +
@@ -39,4 +42,39 @@ cov_sw_bar <- ggplot(aes(x = var2, y = importance), data = cov_sw) +
 sw_combo <- ggarrange(rich_sw_bar, cov_sw_bar, nrow = 1, ncol = 2, labels = c("a", "b"))
 ggsave("sw_bar.png", plot = sw_combo, path = "Figures", height = 1000, width = 2000, units = 'px')
 
+####GRID OF VARIABLE EFFECTS ON NINTC####
+#import modelling data
+modeldat <- read.csv("Functional trait data\\Clean data\\nint_nurse_traits.csv", row.names = 1) 
+modeldat$nurse_sp <- as.factor(modeldat$nurse_sp)
+modeldat$graz <- as.factor(modeldat$graz)
+modeldat$site_ID <- as.factor(modeldat$site_ID)
+modeldat$ID <- as.factor(modeldat$ID)
 
+##Add the other environmental covariates to modeldat final
+#import siteinfo, we will use this to add ID to drypop
+siteinfo <- read.csv("C:\\Users\\imke6\\Documents\\Msc Projek\\Facilitation analysis clone\\Facilitation data\\BIODESERT_sites_information.csv") |> 
+  mutate(plotref = str_c(SITE, PLOT, sep = "_")) |> 
+  dplyr::select(ID, plotref, Lat_decimal, Long_decimal) |> 
+  distinct() |> 
+  na.omit()
+
+#import drypop, so which contains the env covariates
+drypop <- read.csv("C:\\Users\\imke6\\Documents\\Msc Projek\\Functional trait analysis clone\\Functional trait data\\Raw data\\drypop_20MAy.csv") |> 
+  mutate(plotref = str_c(Site, Plot, sep = "_")) |> #create a variable to identify each plot
+  dplyr::select(plotref, AMT, RAI, RASE, pH.b, SAC.b) |> 
+  distinct() |> 
+  left_join(siteinfo, by = "plotref") |> 
+  dplyr::select(!plotref)
+drypop$ID <- as.factor(drypop$ID)
+
+#join the env covariates to the nurse nint data
+modeldat_final <- modeldat |> 
+  inner_join(drypop, by = "ID") |> 
+  rename(pH = "pH.b", SAC = "SAC.b") |> 
+  mutate(AMT2 = AMT^2, 
+         aridity2 = aridity^2, 
+         sin_lat = sin(Lat_decimal), #transform from a circular to a linear variable
+         sin_long = sin(Long_decimal)) |> 
+  #remove all rows which have NA values in any of our modelling variables
+  drop_na(NIntc_richness_binom, NIntc_cover_binom,NInta_richness_binom, NInta_cover_binom, 
+          log_nurse_meanLDMC, log_nurse_meanH, aridity, AMT, RASE, SAC, pH, graz)
