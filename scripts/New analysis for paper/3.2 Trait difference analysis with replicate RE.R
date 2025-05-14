743724787d912a6eec6ae92e78519b3c6eb334ad
ImkeSmit <imke610@gmail.com>
2025-05-14 15:14:42 +0200

    note end time

--- scripts/New analysis for paper/3.2 Trait difference analysis with replicate RE.R
+++ scripts/New analysis for paper/3.2 Trait difference analysis with replicate RE.R
@@ -84,7 +84,7 @@ maxh_full_model <- glmmTMB(formula = full_formula, data = maxh_data)
 options(na.action = "na.fail")
 maxh_model_selection <- dredge(maxh_full_model, 
                                fixed = c("cond(sin_lat)","cond(sin_long)"), #random effects are automatically included in all models due to the structure of tMB
-                               rank = "AIC") #start 13:39, end 13:46
+                               rank = "AIC") #start 14:47 end 15:14
 
 #get the bets models
 maxh_eq_models <- get.models(maxh_model_selection, subset= delta <2) #2 equivalent models
