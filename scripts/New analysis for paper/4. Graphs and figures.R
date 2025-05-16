b981359280020f594f64f42209f9a1f92a812a52
ImkeSmit <imke610@gmail.com>
2025-05-16 12:36:32 +0200

    change importance after reruninng

--- scripts/New analysis for paper/4. Graphs and figures.R
+++ scripts/New analysis for paper/4. Graphs and figures.R
@@ -2041,7 +2041,7 @@ H_ass_SAC <- ggplot(maxh_data, aes(x = SAC, y = trait_difference)) +
   scale_fill_manual(labels = c("bare-associated", "dominant-associated"),
                     values = c(brewer.pal(8, "Dark2")[7], brewer.pal(8, "Dark2")[1])) +
   guides(fill = "none") +
-  annotate("text", x = Inf, y = Inf, label = "Importance = 0.54", hjust= 1, vjust = 1, size = 5)+
+  annotate("text", x = Inf, y = Inf, label = "Importance = 0.45", hjust= 1, vjust = 1, size = 5)+
   theme(axis.title = element_text(size = 18), axis.text = element_text(size = 12), 
         legend.text = element_text(size = 16))
 
